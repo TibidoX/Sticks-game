eba41a97cf7cc411596220921e211f5847f6dc56
TibidoX <vovaboch0@gmail.com>
2023-10-30 23:04:59 +0300

    Draw field and horizontal lines

---
+++ src/main/java/com/mycompany/sticks/MyPoint.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sticks;
+
+/**
+ *
+ * @author vovab
+ */
+public class MyPoint {
+    private int x = 0, y = 0;
+    
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    int getX() {
+        return x;
+    }
+    
+    int getY() {
+        return y;
+    }
+}
---
+++ src/main/java/com/mycompany/sticks/Sticks.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sticks;
+
+/**
+ *
+ * @author vovab
+ */
+public class Sticks {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
